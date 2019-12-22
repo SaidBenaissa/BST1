ada1c0c16aa12c394ad693eb051ac8d320045028
SaidBenaissa <d.sbenaissa@gmail.com>
2019-12-22 17:19:40 +0100

    dirs that excluded added

---
+++ cmake-build-debug/CMakeFiles/BST1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BST1"
+  "BST1.pdb"
+  "CMakeFiles/BST1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BST1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
